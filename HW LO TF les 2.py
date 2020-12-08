37de47f4a8700389aac34b79c2f6db8cdec294a2
AleksandrD <aleksandrdudnyk1991@gmail.com>
2020-12-08 10:07:52 +0200

    logical operations

---
+++ HW LO TF les 2.py
@@ -0,0 +1,8 @@
+a = int(input("Please, enter the first value: "))
+b = int(input("Please, enter the second value: "))
+x = int(input("Please, enter the third value: "))
+
+if a < x < b:
+    print(True)
+else:
+    print(False)
